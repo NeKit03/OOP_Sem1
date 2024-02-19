ee35eddf2a6d4d79d17f93673051c00d61c8f872
Nikita <nikita91797@gmail.com>
2024-02-19 15:03:33 +0800

    add sem1

---
+++ VendingMachine.java
@@ -0,0 +1,19 @@
+package org.example.sem1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface VendingMachine {
+
+
+    Product sellProduct(String name);
+
+
+
+    double getPrice(String name);
+
+
+
+
+
+}
